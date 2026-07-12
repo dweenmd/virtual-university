8ab27d8a8a7bd7b7a07c209a839b78c387db89c5
dweenmd <dween.md01@gmail.com>
2026-07-12 10:57:01 +0600

    up

--- db.php
+++ db.php
@@ -15,7 +15,7 @@ if ($_SERVER['SERVER_NAME'] === 'localhost') {
     // InfinityFree live server
     $host = "sql103.infinityfree.com";
     $username = "if0_42374690";
-    $password = "32mecproject123"; // <-- put the real password here locally, never commit it
+    $password = "32mecproject123"; // <-- put the real password here locally
     $dbname = "if0_42374690_virtual_university";
     $port = 3306; // InfinityFree MySQL port
 }
